d53d549e2a2984d2c9c47c0f3e183c7b3474c283
fedorkobylkevitch <fedorkobylkevitch@yandex.ru>
2026-04-23 00:39:06 +0300

    update port to 8080

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.main:app",
         host="0.0.0.0",
-        port=1010,
+        port=8080,
         reload=True,
         log_level="info"
     )
